f4a075219297cd7fad744b645e0833bab77c30c9
Noah <Noaheasley408@gmail.com>
2021-05-13 20:16:17 -0500

    Enemy can now stop at the sight of the player
    how repulsive

--- Assets/Scripts/AIBehaviour/EnemyVisionBehaviour.cs
+++ Assets/Scripts/AIBehaviour/EnemyVisionBehaviour.cs
@@ -1,11 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyVisionBehaviour : MonoBehaviour
 {
+    private NavMeshAgent _agent;
     void FixedUpdate()
     {
-        
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+            gameObject.GetComponent<NavMeshAgent>().isStopped = true;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            gameObject.GetComponent<NavMeshAgent>().isStopped = false;
+        }
+    
     }
 }
