14c4edfc27473b980a03b6c6e6328bd5cf83acb2
Noah <noaheasley408@gmail.com>
2021-05-17 10:29:36 -0500

    added comments to code

--- Assets/Scripts/AIBehaviour/EnemyVisionBehaviour.cs
+++ Assets/Scripts/AIBehaviour/EnemyVisionBehaviour.cs
@@ -24,20 +24,23 @@ public class EnemyVisionBehaviour : MonoBehaviour
     }
     void FixedUpdate()
     {
-        // Bit shift the index of the layer (8) to get a bit mask
+        // Bit shift the index of the player layer
         LayerMask layerMask = LayerMask.GetMask("Player");
-
+        //the raycast hit
         RaycastHit hit;
         // If the ray intersects an obect stop the enemy
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
         {
+            //changes the ray to yellow if it hits the player
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Hit");
+            //stops the enemy
             gameObject.GetComponent<NavMeshAgent>().isStopped = true;
         }
         else
         {
+            //enemy's raycast remains white
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            //enemy continues pursuing the player
             gameObject.GetComponent<NavMeshAgent>().isStopped = false;
         }
     
