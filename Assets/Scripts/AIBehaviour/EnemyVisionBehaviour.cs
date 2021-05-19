0c386b0b0273c06d23cb2168363c8b3a65cafebf
Noah <Noaheasley408@gmail.com>
2021-05-19 09:33:40 -0500

    Auto stash before merge of "NoahEasleys-branch" and "origin/NoahEasleys-branch"

--- Assets/Scripts/AIBehaviour/EnemyVisionBehaviour.cs
+++ Assets/Scripts/AIBehaviour/EnemyVisionBehaviour.cs
@@ -35,16 +35,30 @@ public class EnemyVisionBehaviour : MonoBehaviour
         // If the ray intersects an obect stop the enemy
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, _maxVisionDistance, layerMask))
         {
+<<<<<<< Updated upstream
             //changes the ray to yellow if it hits the player
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
             //stops the enemy
+=======
+            //draws the ray
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            //pops up a message on the debug menu stating that the enemy detected something
+            Debug.Log("Did Hit");
+            //stops the nav mesh once the enemy spots the player
+>>>>>>> Stashed changes
             gameObject.GetComponent<NavMeshAgent>().isStopped = true;
         }
         else
         {
+<<<<<<< Updated upstream
             //enemy's raycast remains white
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
             //enemy continues pursuing the player
+=======
+            //draws the ray
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            //resets teh nav mesh agent if the player leaves the enemy's view
+>>>>>>> Stashed changes
             gameObject.GetComponent<NavMeshAgent>().isStopped = false;
         }
     
