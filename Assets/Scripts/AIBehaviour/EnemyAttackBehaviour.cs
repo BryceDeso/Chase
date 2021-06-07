54333c54762759ed621f3ba5eb4574cfe58f6110
Bryce <brycedeso123@gmail.com>
2021-06-07 12:03:35 -0500

    Added player to main scene

--- Assets/Scripts/AIBehaviour/EnemyAttackBehaviour.cs
+++ Assets/Scripts/AIBehaviour/EnemyAttackBehaviour.cs
@@ -15,6 +15,6 @@ public class EnemyAttackBehaviour : MonoBehaviour
 
     public void Attack()
     {
-        weapon.GetComponent<BulletEmitterBehavior>().Shoot();
+        weapon.GetComponent<PlayerBulletEmitterBehavior>().Shoot();
     }
 }
