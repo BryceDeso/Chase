7a259e1992c0a67fc9cd5dd5649ae0edf44f46e4
Noah <noaheasley408@gmail.com>
2021-06-09 12:58:33 -0500

    fixed enemy shooting bug

--- Assets/Scripts/AIBehaviour/EnemyBulletEmitterBehaviour.cs
+++ Assets/Scripts/AIBehaviour/EnemyBulletEmitterBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletEmitterBehavior : MonoBehaviour
+public class EnemyBulletEmitterBehaviour : MonoBehaviour
 {
     [Tooltip("Refernce to the bullet")]
     [SerializeField]
@@ -15,25 +15,20 @@ public class BulletEmitterBehavior : MonoBehaviour
     //Holds a bool to determind wether or not you can shoot again.
     private bool canShoot = true;
 
-    /// <summary>
-    /// Creates an instance of the bullet and applies a force thats multiplied by _bulletSpeed, once the player has shot, 
-    /// canShoot is set to false and a timer is set using Invoke and after a certain amount of time the 
-    /// funtion canShoot is invoked and sets canShoot to true enabling the player to shoot again.
-    /// </summary>
+
     public void Shoot()
     {
-        if(canShoot == true)
+        if (canShoot == true)
         {
             canShoot = false;
 
             Vector3 force = transform.forward * _bulletSpeed;
 
             GameObject bulletFired = Instantiate(_bullet, transform.position, transform.rotation);
-            BulletBehavior bulletscript = bulletFired.GetComponent<BulletBehavior>();
             EnemyBulletBehaviour enemyBulletScript = bulletFired.GetComponent<EnemyBulletBehaviour>();
-            if (bulletscript)
+            if (enemyBulletScript)
             {
-                bulletscript.Rigidbody.AddForce(force, ForceMode.Impulse);
+                enemyBulletScript.Rigidbody.AddForce(force, ForceMode.Impulse);
             }
             Invoke("CanShoot", TimeBetweenShots);
         }
