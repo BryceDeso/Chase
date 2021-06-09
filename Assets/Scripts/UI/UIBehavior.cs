6fcb9758994a07b1187be5756f1e1b0d713d0a5d
Bryce <brycedeso123@gmail.com>
2021-06-09 14:57:22 -0500

    Working on UI

--- Assets/Scripts/UI/UIBehavior.cs
+++ Assets/Scripts/UI/UIBehavior.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private PlayerBehavior PlayerRef;
+
+    public Text scoreText;
+    public Text lifeText;
 
     // Update is called once per frame
     void Update()
     {
-        
+        ActiveText();
+    }
+
+    public void ActiveText()
+    {
+        scoreText.text = "Score: " + PlayerRef.score.ToString();
+
+        lifeText.text = "Lives: " + PlayerRef.lifes.ToString();
     }
 }
