51a623edc90a81aafe79e1138427810447fd9d6f
Jeremy J. Flowers <rpgjuwangaming@gmail.com>
2021-06-11 11:57:52 -0500

    Updated GameManager

--- Assets/Scripts/Gameplay/GameManagerBehavior.cs
+++ Assets/Scripts/Gameplay/GameManagerBehavior.cs
@@ -7,6 +7,21 @@ public class GameManagerBehavior : MonoBehaviour
 {
     public bool gameOver;
 
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GameEngine()
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/UI/UIBehavior.cs
+++ Assets/Scripts/UI/UIBehavior.cs
@@ -6,18 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class UIBehavior : MonoBehaviour
 {
-    [SerializeField]
-    GameObject pauseMenu;
-
-    private void OnPause()
-    {
-        pauseMenu.SetActive(true);
-    }
 
     // Start is called before the first frame update
     void Start()
     {
-        pauseMenu.SetActive(false);
+
     }
 
     // Update is called once per frame
