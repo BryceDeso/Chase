f837ad86f97da3d5e7c141be177137ca3824a426
Bryce Deshotel <brycedeso123@gmail.com>
2021-06-12 13:13:25 -0500

    Made start menu on key press instead a button to ui for gamepad support

--- Assets/Scripts/UI/StartMenuBehavior.cs
+++ Assets/Scripts/UI/StartMenuBehavior.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class StartMenuBehavior : MonoBehaviour
 {
+    private void Update()
+    {
+        StartGame();
+    }
+
     public void StartGame()
     {
-        SceneManager.LoadScene("BryceTest");
+        var keyboard = Keyboard.current;
+
+        if(keyboard.anyKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene("BryceTest");
+        }
     }
 
     public void ExitGame()
